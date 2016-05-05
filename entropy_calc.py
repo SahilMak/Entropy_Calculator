8162d87bcb1d66c8aefbc69f11105b886435b376
Sahil Makhijani <svmakhijani94@gmail.com>
2016-05-05 19:39:32 -0400

    Added import

--- entropy_calc.py
+++ entropy_calc.py
@@ -3,19 +3,21 @@ import sys
 import math
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+import getpass
 
 def open_file():
-  # function
+  # Create Tk object
+  root = Tk()
+  # Hide blank window
+  root.withdraw()
 
 # Get file from command line
 file = open(sys.argv[1])
-
 # Initialize variables
 chars = []
 counts = []
 total = 0
 entropy = 0
-
 # Read file
 for line in file:
   for element in line:
@@ -24,11 +26,9 @@ for line in file:
       counts.append(0)
     counts[chars.index(element)] += 1
     total += 1
-
 # Calculate probabilities
 for x in range(len(chars)):
   probs.append(counts[x] / total)
-
 # Calculate entropy
 for x in range(len(chars)):
   entropy += -probs[x] * math.log(probs[x], 2)
